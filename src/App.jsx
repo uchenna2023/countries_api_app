9c4e344b57838c97395d387a3f1914401caca1a4
Daniel Sarkodie <dsarkodie500@gmail.com>
2023-07-03 13:35:05 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card" style={{ fontSize: "28px" }}>
+        <p>Enjoy building this project</p>
+      </div>
+      <p style={{ fontSize: "24px" }} className="read-the-docs">
+        Countries API project
+      </p>
+    </>
+  );
+}
+
+export default App;
